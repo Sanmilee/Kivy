804cce3c0ca2edbfad34c692aa2230673c9c88d6
Ariyo Sanmi <sanmi4ariyo@gmail.com>
2021-04-22 19:18:32 +0800

    Add Kivy Files

---
+++ KivyMD/main29.py
@@ -0,0 +1,47 @@
+# Data Table: containing rows and colums
+
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+# automatic window size - for mobile testing
+Window.size = (300, 500)
+
+screen_helper = '''
+Screen:
+    BoxLayout:
+        orientation: 'vertical'
+        
+        MDToolbar:
+            title: 'Demo App'
+            left_action_items: [["menu", lambda x: app.navigation_draw()]]
+            right_action_items: [["clock", lambda x: app.navigation_draw()]]
+            elevation: 10    # shadow
+
+        MDLabel:
+            text: 'Hello World LeeAI'
+            halign: 'center'
+
+        MDBottomAppBar
+            MDToolbar:
+                title: 'Help'
+                left_action_items: [["coffee", lambda x: app.navigation_draw()]]
+                mode: 'end'         # 'free-end'
+                type: 'bottom'
+'''
+
+
+class LeeAIApp(MDApp):
+
+    def build(self):
+        self.theme_cls.primary_palette = 'Red'
+
+        screen = Builder.load_string(screen_helper)
+
+        return screen
+
+    def navigation_draw(self):
+        print("Navigation")
+
+
+LeeAIApp().run()
---
+++ KivyMD/main31.py
@@ -0,0 +1,39 @@
+# Navigation Helper
+
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+# automatic window size - for mobile testing
+Window.size = (300, 500)
+
+navigation_helper = '''
+Screen:
+    NavigationLayout:
+        ScreenManager:
+            Screen:
+
+                BoxLayout:
+                    orientation: 'vertical'
+                    
+                    MDToolbar:
+                        title: 'Demo App'
+                        left_action_items: [["menu", lambda x: nav_drawer.toggle_nav_drawer()]]
+                        elevation: 10   
+
+                    Widget:
+                    
+        MDNavigationDrawer:
+            id: nav_drawer
+'''
+
+
+class LeeAIApp(MDApp):
+
+    def build(self):
+        screen = Builder.load_string(navigation_helper)
+
+        return screen
+
+
+LeeAIApp().run()
